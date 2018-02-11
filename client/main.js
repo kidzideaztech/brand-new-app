5ad371c5ec35131762be2080a0715f5474129fb8
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-02-10 21:17:36 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,22 +1,15 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
+Template.hello.onCreated(function () {
   this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
+  counter: function () {
     return Template.instance().counter.get();
   },
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+  'click button': function (e, t) {
+    t.counter.set(t.counter.get() + 1);
   },
 });
