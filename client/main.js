7b12eee9507d8d9b52b6ee44bba9599560255b92
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-03-05 18:02:08 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,17 +1,13 @@
-Template.hello.onCreated(function () {
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
+Template.home.helpers({
   leaderboard: function () {
     return Clicks.find({}, {"sort":{"amnt":1}}).fetch();
   },
 });
 
-Template.hello.events({
+Template.home.events({
   'click button': function (e, t) {
     name = $(e.currentTarget).text();
     if(!Clicks.findOne({"name":name})) Clicks.insert({"name":name, "amnt":0});
-    Clicks.update({_id:Clicks.findOne({"name":name})._id}, {$inc:{"amnt":0}});
+    Clicks.update({_id:Clicks.findOne({"name":name})._id}, {$inc:{"amnt":1}});
   },
 });
