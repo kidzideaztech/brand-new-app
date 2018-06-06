ff1b32855849666b2ce68c31fa62dfcc94a10273
Nathaniel Dsouza <nate@kidzideaz.xyz>
2018-06-06 11:30:05 -0400

    Delete register method and instead use validateNewUser

--- server/main.js
+++ server/main.js
@@ -4,24 +4,22 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
-Meteor.methods({
+Meteor.validateNewUser(function (options) {
+  username = options.username;
+  password = options.password;
+  email = options.email;
   
-  register: function (username, password, email) {
-    check(username, String);
-    check(password, String);
-    check(email, String);    
+  check(username, String);
+  check(password, String);
+  check(email, String);    
     
-    if(Accounts.findUserByUsername(username)) throw new Meteor.Error("validation-error", "Username is already in use");
-    if(Accounts.findUserByEmail(email)) throw new Meteor.Error("validation-error", "Email is already in use");    
-    if(password.length < 6) throw new Meteor.Error("validation-error", "Passwords must be at least 6 characters long");
-    
-    Accounts.createUser({
-      "username":username,
-      "email":email,
-      "password":password
-    });
-    
-    return true;
-  }
+  if(Accounts.findUserByUsername(username)) throw new Meteor.Error("validation-error", "Username is already in use");
+  if(Accounts.findUserByEmail(email)) throw new Meteor.Error("validation-error", "Email is already in use");    
+  if(password.length < 6) throw new Meteor.Error("validation-error", "Passwords must be at least 6 characters long");
+
+  return true;  
+});
+
+Meteor.methods({
   
 });
